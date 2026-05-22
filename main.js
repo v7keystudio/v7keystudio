f816cf207a9a23bccdb66a6fb7b2452ab1d12ff4
Anurag Yadav <anurag6200633@gmail.com>
2026-05-22 07:54:30 +0530

    404 error

---
+++ main.js
@@ -0,0 +1,37 @@
+/* Menu Mobile */
+
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
+
+if(navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu');
+    })
+}
+
+if(navClose){
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+    })
+}
+
+const navLink = document.querySelectorAll('.nav-link');
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show-menu');
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+/* Scroll Reveal */
+
+const sr = ScrollReveal({
+    distance: '90px',
+    duration: 3000,
+})
+
+sr.reveal(`.home-data`, {origin: 'top', delay: 400});
+sr.reveal(`.home-img`, {origin: 'bottom', delay: 600});
+sr.reveal(`.home-footer`, {origin: 'bottom', delay: 800})
